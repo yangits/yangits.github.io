73cda1fc4f0cd884e069a6147faf2c2a2374f834
yangits <934550145@qq.com>
2023-12-13 15:13:28 +0800

    main

--- hosts.py
+++ hosts.py
@@ -1,28 +1,43 @@
 import requests  
 import re,os
+urls=["github.com","maxcdn.bootstrapcdn.com","api.github.com","github.io"]
 url_hosts = 'https://raw.hellogithub.com/hosts'  
-url_github = 'https://site.ip138.com/github.com/'  
+hosts=""""""
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/119.0.0.0"}  
 # 在请求头中带上User-Agent，模拟浏览器发送请求  
-github_html = requests.get(url_github, headers=headers).text
-hosts = requests.get(url_hosts, headers=headers).text
-github_ip=re.findall('/" target="_blank">([0-9]*.[0-9]*.[0-9]*.[0-9]*)</a>',github_html)
-for ip in github_ip:
-    with open("C:/Windows/System32/drivers/etc/hosts", "w") as f:
-        f.write(ip+"        github.com\n")
-        f.write(hosts)
-    f.close()
-    dos = "ipconfig /flushdns"
-    cmd = os.system(dos)
-    print("***********************************")
-    try:
-        github = requests.get("https://github.com/", headers=headers,timeout=30)
-        if github.status_code==200:
-            print(ip+"登录GitHub.coom成功")
-            break
-        else:
-            print(ip+"登录GitHub.coom失败")
-    except:
-        print(ip+"登录GitHub.coom失败")
-
-
+hosts_hello = requests.get(url_hosts, headers=headers).text
+for url in urls:
+    url_ip138 = 'https://site.ip138.com/'  + url
+    ip138_html = requests.get(url_ip138, headers=headers).text
+    ip138_ips=re.findall('/" target="_blank">([0-9]*.[0-9]*.[0-9]*.[0-9]*)</a>',ip138_html)
+    for ip in ip138_ips:
+        with open("C:/Windows/System32/drivers/etc/hosts", "w") as f:
+            f.write(ip+"        "+url+"\n")
+        f.close()
+        dos = "ipconfig /flushdns"
+        cmd = os.system(dos)
+        print("***********************************")
+        # dos_url = "ping "+url
+        # cmd_url = os.system(dos_url)
+        # if cmd_url == 0:
+        #     print(" ping "+ url + ip + "成功")
+        #     hosts = hosts +ip+"""        """+url+"""\n"""
+        #     break
+        # else:
+        #     print(" ping "+ url + ip + "失败...")                
+        try:
+            github = requests.get("https://"+url, headers=headers,timeout=15)
+            if github.status_code==200:
+                print("登录 "+url+" ["+ip+"] 成功")
+                hosts = hosts +ip+"""        """+url+"""\n"""
+                break
+            else:
+                print("登录 "+url+" ["+ip+"] 失败...")
+        except:
+            print("登录 "+url+" ["+ip+"] 失败...")
+with open("C:/Windows/System32/drivers/etc/hosts", "w") as f:
+    f.write(hosts)
+    f.write(hosts_hello)
+f.close()
+dos = "ipconfig /flushdns"
+cmd = os.system(dos)
---
+++ online_chat/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, join_room, leave_room, send
+
+app = Flask(__name__)
+app.secret_key = 'secret'
+socketio = SocketIO(app)
+
+# 主页路由
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# 用户加入聊天室事件
+@socketio.on('join', namespace='/chat')
+def join(message):
+    room = message['room']
+    join_room(room)
+    send({'msg': message['username'] + " 加入了聊天室."}, room=room)
+
+# 用户发送消息事件
+@socketio.on('text', namespace='/chat')
+def text(message):
+    room = message['room']
+    send({'msg': message['username'] + ": " + message['msg']}, room=room)
+
+# 用户离开聊天室事件
+@socketio.on('left', namespace='/chat')
+def left(message):
+    room = message['room']
+    leave_room(room)
+    send({'msg': message['username'] + " 离开了聊天室."}, room=room)
+
+if __name__ == '__main__':
+    socketio.run(app)
