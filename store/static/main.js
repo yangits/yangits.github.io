b547ddbde6f575c3b30d385887437b32fa97c2e9
yangits <934550145@qq.com>
2024-01-11 11:53:37 +0800

    main

--- store/static/main.js
+++ store/static/main.js
@@ -1,67 +1,59 @@
 var username= document.getElementById("username")
 var password= document.getElementById("password")
+var storename = document.getElementById("storename");
 var timsg= document.getElementById("timsg")
 var login = document.getElementById("login");
-var storename = document.getElementById("storename");
 var text = document.getElementById("text");
 var goods_table = document.getElementById("goods_table");
 var select_name = document.getElementById("select_name");
 var select_specs = document.getElementById("select_specs");
-var xmlhttpmsg;
+var xhr;
 if (window.XMLHttpRequest){// code for IE7+, Firefox, Chrome, Opera, Safari
-    xmlhttpmsg=new XMLHttpRequest();
+    xhr=new XMLHttpRequest();
 }else{// code for IE6, IE5
-    xmlhttpmsg=new ActiveXObject("Microsoft.XMLHTTP");
+    xhr=new ActiveXObject("Microsoft.XMLHTTP");
 }
-if (localStorage.getItem('username') && localStorage.getItem('password')) {
-    username.value = (localStorage.getItem('username'))
-    password.value = (localStorage.getItem('password')) // hideLogin()
-    Login()
+password.value=="";select_name.value="";select_specs.value="";
+if (localStorage.getItem('username') && localStorage.getItem('storename')) {
+    username.options[localStorage.getItem('username')].selected = true;
+    storename.options[localStorage.getItem('storename')].selected = true;
 }
-
 function Login(){
     if ( username.value == "" ){
         timsg.innerHTML="用户名不能为空！"
-        localStorage.setItem('username', username.value);
-        localStorage.setItem('password', password.value);
     }else{
         timsg.innerHTML=""
-        localStorage.setItem('username', username.value);
-        localStorage.setItem('password', password.value);
-        hideLogin()
     }
+    localStorage.setItem('username', username.selectedIndex);
+    localStorage.setItem('storename', storename.selectedIndex);
 }
-
 function showLogin() { // 显示登录窗口的函数
     login.style.display = "block";
 }
-
 function hideLogin() { // 隐藏登录窗口的函数
     login.style.display = "none";
 }
-
 function new_add(){
+    Login()
     if (storename.value==""){
         alert("仓库名称不能为空")
     }else{
-        msg="/new_add?storename="+storename.value
-        xmlhttpmsg.open("post",msg,true);
-        xmlhttpmsg.send();
-        xmlhttpmsg.onreadystatechange = function(){
-            if (xmlhttpmsg.readyState==4){
-                if(xmlhttpmsg.status==200)
-                    if  (xmlhttpmsg.responseText=="open"){
-                        alert("已打开仓库")
-                        get_goods()
+        msg="/new_add"
+        xhr.open("post",msg,true);
+        var fdata = new FormData();
+        fdata.append('storename', storename.value);
+        xhr.send(fdata);
+        xhr.onreadystatechange = function(){
+            if (xhr.readyState==4){
+                if(xhr.status==200)
+                    if  (xhr.responseText=="open"){
+                        get_goods();hideLogin()
                     }else{
-                        if(xmlhttpmsg.responseText=="error"){
+                        if(xhr.responseText=="error"){
                         alert("仓库名不合法（不能数字开头，不能包含空格），请重新输入")
                     }else{                        
-                        alert("仓库不存在，新<"+storename.value+">创建成功")
-                        select_name.value="";
-                        select_specs.value="";
-                        get_goods()}
-
+                        alert("仓库不存在，仓库名<"+storename.value+">创建成功")
+                        get_goods();hideLogin()}
                     }
                 else{
                     alert("服务器未开启")
@@ -71,23 +63,20 @@ function new_add(){
     }
 }
 function get_goods(){
-    msg="/goods?storename="+storename.value+"&select_name="+select_name.value+"&select_specs="+select_specs.value
-    xmlhttpmsg.open("post",msg,true);
-    xmlhttpmsg.send();
-    xmlhttpmsg.onreadystatechange = function(){
-        if (xmlhttpmsg.readyState==4){
-            console.log(xmlhttpmsg.responseText);
-            if(xmlhttpmsg.status==200){
-                goods_table.innerHTML="<tr>"+
-                "<td width=50px>序号</td>"+
-                "<td width=100px>名称</td>"+
-                "<td width=200px>规格</td>"+
-                "<td width=100px>备注</td>"+
-                "<td width=50px>价格</td>"+
-                "<td width=100px>数量</td>"+
-                "<td width=150px>操作</td>"+
-                "</tr>"
-                var goods_lists=JSON.parse(xmlhttpmsg.responseText)
+    msg="/goods"//"/goods?storename="+storename.value;
+    xhr.open("post",msg,true);
+    var data = new FormData();
+    data.append('storename', storename.value);
+    data.append('select_name', select_name.value);
+    data.append('select_specs', select_specs.value);
+    xhr.send(data);
+    xhr.onreadystatechange = function(){
+        if (xhr.readyState==4){
+            console.log(xhr.responseText);
+            if(xhr.status==200){
+                goods_table.innerHTML="<tr>"+"<td>序号</td>"+"<td>名称</td>"+"<td>规格</td>"+
+                "<td>备注</td>"+"<td>价格</td>"+"<td>数量</td>"+"<td>操作</td>"+"</tr>"
+                var goods_lists=JSON.parse(xhr.responseText)
                 for (let i = 0; i < goods_lists.length; i++) {   
                     var tr = document.createElement("tr");
                     for (let j = 0; j < goods_lists[0].length; j++) {
@@ -117,17 +106,20 @@ function up_excel(){
 }
 function file_excel(){
     var file_excel= document.getElementById("file_excel");
-    msg="/up_excel";
+    msg="/up_excel?storename="+storename.value;
     var fdata = new FormData();
     fdata.append('up_file_excel', file_excel.files[0]);
-    xmlhttpmsg.open("post",msg,true);
-    xmlhttpmsg.send(fdata);
-    xmlhttpmsg.onreadystatechange = function(){
-        if (xmlhttpmsg.readyState==4){
-            if(xmlhttpmsg.status==200){
-                if  (xmlhttpmsg.responseText=="success"){
+    xhr.open("post",msg,true);
+    xhr.send(fdata);
+    
+    xhr.onreadystatechange = function(){
+        if (xhr.readyState==4){
+            if(xhr.status==200){
+                if  (xhr.responseText=="success"){
                     alert(file_excel.files[0].name+"上传成功")
-                }
+                }else{alert(file_excel.files[0].name+"上传失败")}
             }else{alert("上传失败,服务器响应错误")}
+            file_excel.value=null
         }}
+
 }
--- store/store.py
+++ store/store.py
@@ -2,6 +2,7 @@
 import sqlite3
 
 from flask import Flask, jsonify, redirect, render_template, request
+
 app = Flask(__name__)
 
 app.json.ensure_ascii = False
@@ -13,22 +14,20 @@ def login():
         return render_template("main.html")
 @app.route('/new_add', methods = ['POST'])
 def create_db():
-    storename=request.args["storename"]
+    storename=request.form["storename"]
     conn_data = sqlite3.connect('store.db') 
     cur_data = conn_data.cursor()  
     cur_data.execute("SELECT name FROM sqlite_master WHERE type='table'")
     tables = cur_data.fetchall() # Tables 为元组列表
     if (storename,) not in tables:
         try:
-            cur_data.execute(f'create table  {storename} (id integer primary key autoincrement ,名称 text ,规格 text ,备注 text,价格 text,数量 float)')
-            for i in range(1,50):
-                cur_data.execute(f'insert into {storename}(名称,规格,备注,价格,数量) values(?,?,?,?,?)',("螺栓",f"M10*{i*10}","",f"{i*1}",i*100))
-            for i in range(1,50):
-                cur_data.execute(f'insert into {storename}(名称,规格,备注,价格,数量) values(?,?,?,?,?)',("燕尾丝",f"M10*{i*10}","",f"{i*1}",i*100))
+            cur_data.execute(f'create table  {storename} (id integer primary key autoincrement ,名称 text ,规格 text ,备注 text,价格 text,数量 text)')
+            for i in range(1,20):
+                cur_data.execute(f'insert into {storename}(名称,规格,备注,价格,数量) values(?,?,?,?,?)',("外六角螺栓",f"M10*{i*10}","",f"{i*1}",i*100))
             conn_data.commit() 
             cur_data.close() 
             conn_data.close() 
-            return tables
+            return "new_add"
         except:
             return "error"
     else:
@@ -37,9 +36,10 @@ def create_db():
         return "open"
 @app.route("/goods" , methods=['post'])# 连接
 def goods():
-    storename=request.args["storename"]
-    select_name=request.args["select_name"]
-    select_specs=request.args["select_specs"]
+    # storename=request.args["storename"]
+    storename=request.form["storename"] 
+    select_name=request.form["select_name"]
+    select_specs=request.form["select_specs"]
     conn_data = sqlite3.connect('store.db') 
     cur_data = conn_data.cursor()  
     cur_data.execute(f"select * from {storename} where 名称 like ? and 规格 like ?",("%"+select_name+"%","%"+select_specs+"%"))
@@ -49,14 +49,29 @@ def goods():
     return data
 @app.route("/up_excel" , methods=['post'])# 连接
 def up_excel():
-
+    storename=request.args["storename"]
+    # print(storename)
     up_excel=request.files["up_file_excel"]
-    print(up_excel)        
-    # for each in up_excel.readlines():
-    #     print(each)
-    # for row in up_excel.readlines().decode("utf-8"):
-        # print(row)
-    return "success"
+    up_excel_str=up_excel.read().decode("utf-8")
+    up_excel.save("d:/"+up_excel.filename)
+    conn_data = sqlite3.connect('store.db') 
+    cur_data = conn_data.cursor() 
+    # print(up_excel_str.splitlines()[1])
+    if up_excel_str.splitlines()[0]!="名称,规格,备注,价格,数量":
+        try:
+            for each in up_excel_str.splitlines()[1:]:
+                aaa=each.split(",")
+                cur_data.execute(f'insert into {storename}(名称,规格,备注,价格,数量) values(?,?,?,?,?)',(aaa[0],aaa[1],aaa[2],aaa[3],aaa[4]))
+            conn_data.commit() 
+            cur_data.close() 
+            conn_data.close() 
+            return "success"
+        except:
+            return "error"
+    else: 
+        return "error"
+
+    
     
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=888)
